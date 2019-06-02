f680af63272e9467d118afdcfe9531b7b3290cc3
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-02 21:52:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 // write your code below!
-Function happyHolidays(Happy Holidays) { return Happy Holidays! }
-Function happyHolidaysTo(name) 
-{return `Happy holidays, ${name}`}
-Function happyHolidayTo(holiday, name) {return `${holiday}, ${name}` }
-Function holidayCountdown(holiday,days) { return `It's ${days} until ${holiday}`} 
+Function happyHolidays(string)
