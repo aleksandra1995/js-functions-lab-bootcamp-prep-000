2ca46ae5e01c0899de1df13464a7ca89ff843b16
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-02 21:49:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // write your code below!
-Function happyHolidays() { return Happy Holidays! }
+Function happyHolidays(Happy Holidays) { return Happy Holidays! }
 Function happyHolidaysTo(name) 
 {return `Happy holidays, ${name}`}
 Function happyHolidayTo(holiday, name) {return `${holiday}, ${name}` }
