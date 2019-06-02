5a11f65b7c43c34bdeaea7003957ec936776ecc4
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-02 22:19:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,3 @@ function happyHolidayTo(holiday,name) {
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!` 
 }
-function happyHolidays(string) {
-  return Happy holidays!
-}
